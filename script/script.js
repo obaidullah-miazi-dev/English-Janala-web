8a506b0a10382bfbf1c6a783651cc6ac209fb648
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-03 23:11:24 +0600

    active button toggling feature added

--- script/script.js
+++ script/script.js
@@ -11,27 +11,32 @@ const getLesson = (lesson) => {
     for (const lessons of lesson){
         const newDiv = document.createElement('div')
     newDiv.innerHTML = `
-    <button onclick="loadWord(${lessons.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson- ${lessons.level_no}</button>
+    <button id="lesson-btn-${lessons.level_no}" onclick="loadWord(${lessons.level_no})" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i> Lesson- ${lessons.level_no}</button>
     `
 
     lessonContainer.append(newDiv)
     }
 }
 
+const removeActive=()=>{
+    const lessonBtn= document.querySelectorAll('.lesson-btn')
+        lessonBtn.forEach(btn=>btn.classList.remove('active'))
+}
+
 const loadWord= (id)=>{
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
     .then(res=> res.json())
-    .then(data => displayWord(data.data))
+    .then(data => {
+        removeActive()
+        const clickBtn= document.getElementById(`lesson-btn-${id}`)
+        clickBtn.classList.add('active')
+        displayWord(data.data)
+    })
 }
 
-// {
-//     "id": 5,
-//     "level": 1,
-//     "word": "Eager",
-//     "meaning": "আগ্রহী",
-//     "pronunciation": "ইগার"
-// }
+            
+
 
 const displayWord = (words) =>{
     const wordContainer= document.getElementById('word-container')
